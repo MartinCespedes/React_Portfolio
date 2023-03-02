efd88e6d166706c5f29c0d2e4dcfb8ab5432de7c
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-02 11:54:23 -0500

    Finished About.jsx

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import './about.css'
-
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 const About = () => {
   return (
    <section id="about">
-    About
-   </section>
+    <h5>Get To Know</h5>
+    <h2>About Me</h2>
+
+
+    <div className='.container about__container'>
+      <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt="About Image" />
+        </div>
+      </div>
+
+      <div className="about__content">
+        <div className="about__cards">
+          <article className="about__card">
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>10+ Years Working</small>
+
+          </article>
+          <article className="about__card">
+            <FiUsers className='about__icon'/>
+            <h5>Clients</h5>
+            <small>100+ Worldwide</small>
+          </article>
+          <article className="about__card">
+            <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small>25+ Completed</small>
+            </article>
+          </div>
+          <p>
+          Hello, I'm Martin Cespedes, a driven full-stack developer with a passion for creating impactful projects that solve real-world problems. With a commitment to lifelong learning and a track record of delivering high-quality software solutions, I am eager to bring my skills and expertise to a dynamic and fast-paced software development environment.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
+  </section>
   )
 }
 
