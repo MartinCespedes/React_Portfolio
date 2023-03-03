c5e90216eba5940478ad95ff17a631ad591e4eba
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-02 21:01:50 -0500

    Finished Portfolio.jsx & added const data array

--- react-portfolio/src/components/Portfolio/Portfolio.jsx
+++ react-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -7,6 +7,52 @@ import IMG4 from '../../assets/E-Commerce_IMG1.png'
 import IMG5 from '../../assets/Web_Quiz_1.png'
 import IMG6 from '../../assets/project_development.png'
 
+
+const data = [
+  {
+    id:1,
+    image: IMG1,
+    title: 'Beer Buddy',
+    github:'https://github.com/Daphnetatertot/Project_DAAM',
+    demo: 'https://daphnetatertot.github.io/Project_DAAM/'
+  },
+  {
+    id:2,
+    image: IMG2,
+    title: 'Project GameList',
+    github:'https://github.com/MartinCespedes/project-gamelist',
+    demo: 'https://project-gamelist.herokuapp.com/'
+  },
+  {
+    id:3,
+    image: IMG3,
+    title: 'Guess My Number',
+    github:'https://github.com/MartinCespedes/Guess_My_Number',
+    demo: 'https://martincespedes.github.io/Guess_My_Number/'
+  },
+  {
+    id:4,
+    image: IMG4,
+    title: 'E-Commerce Backend',
+    github:'https://github.com/MartinCespedes/E-Commerce_Back_End',
+    demo: 'https://project-gamelist.herokuapp.com/'
+  },
+  {
+    id:5,
+    image: IMG5,
+    title: 'Web API Quiz',
+    github:'https://github.com/MartinCespedes/Web_API_Quiz',
+    demo: 'https://martincespedes.github.io/Web_API_Quiz/'
+  },
+  {
+    id:6,
+    image: IMG6,
+    title: 'Project in Development',
+    github:'https://github.com/MartinCespedes/Project_3',
+    demo: 'https://github.com/MartinCespedes/Project_3'
+  },
+]
+
 const Portfolio = () => {
   return (
 <section id="portfolio">
@@ -14,66 +60,25 @@ const Portfolio = () => {
     <h2>Portfolio</h2>
     
     <div className="container portfolio__container">
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={IMG1} alt="" />
-          </div>
-          <h3>Beer Buddy</h3>
-          <div className='portfolio__item-cta'>
-          <a href="https://github.com/Daphnetatertot/Project_DAAM" className='btn' target='_blank'>Github</a>
-          <a href="https://daphnetatertot.github.io/Project_DAAM/" className='btn btn-primary' target='_blank'>Live Application</a>
-         </div>
-        </article>
-        <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={IMG2} alt="" />
-          </div>
-          <h3>Project GameList</h3>
-          <div className='portfolio__item-cta'>
-          <a href="https://github.com/MartinCespedes/project-gamelist" className='btn' target='_blank'>Github</a>
-          <a href="https://project-gamelist.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Application</a>
-         </div>
-        </article>
-        <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={IMG3} alt="" />
-          </div>
-          <h3>Guess My Number</h3>
-          <div className='portfolio__item-cta'>
-          <a href="https://github.com/MartinCespedes/Guess_My_Number" className='btn' target='_blank'>Github</a>
-          <a href="https://martincespedes.github.io/Guess_My_Number/" className='btn btn-primary' target='_blank'>Live Application</a>
-         </div>
-        </article>
-        <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={IMG4} alt="" />
-          </div>
-          <h3>E-Commerce Back End</h3>
-          <div className='portfolio__item-cta'>
-          <a href="https://github.com/MartinCespedes/E-Commerce_Back_End" className='btn' target='_blank'>Github</a>
-          <a href="https://github.com/MartinCespedes/E-Commerce_Back_End" className='btn btn-primary' target='_blank'>Live Demo</a>
-         </div>
-        </article>
-        <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={IMG5} alt="" />
-          </div>
-          <h3>JavaScript API Quiz</h3>
-          <div className='portfolio__item-cta'>
-          <a href="https://github.com/MartinCespedes/Web_API_Quiz" className='btn' target='_blank'>Github</a>
-          <a href="https://martincespedes.github.io/Web_API_Quiz/" className='btn btn-primary' target='_blank'>Live Demo</a>
-         </div>
-        </article>
-        <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={IMG6} alt="" />
-          </div>
-          <h3>Project In Development</h3>
-          <div className='portfolio__item-cta'>
-          <a href="https://github.com/MartinCespedes/Project_3" className='btn' target='_blank'>Github</a>
-          <a href="https://github.com/MartinCespedes/Project_3" className='btn btn-primary' target='_blank'>Live Demo</a>
-         </div>
-        </article>
+      {
+        data.map(({id, image, title, github, demo}) => {
+          return (  
+          <article key={id} className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className='portfolio__item-cta'>
+            <a href={github} className='btn' target='_blank'>Github</a>
+            <a href={demo} className='btn btn-primary' target='_blank'>Live Application</a>
+           </div>
+          </article>
+
+          )
+        })
+      }
+     
+       
     </div>
   </section>
   )
